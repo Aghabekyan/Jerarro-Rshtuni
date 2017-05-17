6f91e9b0b9c5517e9ca21be8d6504bef018b3eb0
Sargis A <sargis@kinetics-sdm.com>
2017-05-17 16:02:02 +0400

    url static

--- jerarro/urls.py
+++ jerarro/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = patterns('',
-                       # Examples:
-                       url(r'^$', 'homepage.views.index', name='index'),
-                       url(r'^catalog/(?P<category>[male|female]+)/$',
-                           'homepage.views.catalog', name='catalog'),
-                       url(r'^admin/', include(admin.site.urls)),
-                       ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    patterns(url(r'', 'homepage.views.index', name='index'))
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +\
+    patterns('',
+             # Examples:
+             url(r'^$', 'homepage.views.index', name='index'),
+             url(r'^catalog/(?P<category>[male|female]+)/$',
+                 'homepage.views.catalog', name='catalog'),
+             url(r'^admin/', include(admin.site.urls)),
+             url(r'', 'homepage.views.index', name='index')
+             )
