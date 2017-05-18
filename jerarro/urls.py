eec1bafd97a3bdfd7bb8c3a7a344c5e6a137da9b
Sargis A <sargis@kinetics-sdm.com>
2017-05-18 18:08:01 +0400

    11 img

--- jerarro/urls.py
+++ jerarro/urls.py
@@ -8,9 +8,9 @@ urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + 
     patterns('',
              # Examples:
              url(r'^$', 'homepage.views.index', name='index'),
-             url(r'^catalog/(?P<category>[male|female]+)/$',
+             url(r'^catalog/(?P<category>[male|female]+)$',
                  'homepage.views.catalog', name='catalog'),
              url(r'^contacts/post/$', 'homepage.views.contacts_post', name='contacts-post'),
-             url(r'^admin/', include(admin.site.urls)),
+             url(r'^admin', include(admin.site.urls)),
              url(r'', 'homepage.views.index', name='index')
              )
