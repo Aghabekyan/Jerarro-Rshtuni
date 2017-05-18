e02a76b152135a13238e16dcaaa49c56059452f6
USer <user@ubuntu.ubuntu-domain>
2017-05-18 23:57:56 +0400

    collectstatic

--- jerarro/settings.py
+++ jerarro/settings.py
@@ -39,10 +39,7 @@ TEMPLATES = [
     },
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "homepage/static"),
-)
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
@@ -70,6 +67,8 @@ INSTALLED_APPS = (
     'homepage'
 )
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
